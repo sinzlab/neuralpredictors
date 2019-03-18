fbc460edb6f0bac4c437f36cd7c806ca8d28fc8f
kklurz <kk.lurz@gmail.com>
2019-03-18 16:41:21 +0100

    tqdm fix

--- mlutils/layers/readouts.py
+++ mlutils/layers/readouts.py
@@ -4,7 +4,38 @@ import torch
 from torch import nn as nn
 from torch.nn import Parameter
 from torch.nn import functional as F
-from torch.nn import ModuleDict
+#from torch.nn import ModuleDict
+
+class ModuleDict(nn.Module):
+    def __init__(self, modules=None):
+        super().__init__()
+        if modules is not None:
+            for k, v in modules.items():
+                if k not in self._modules:
+                    self.add_module(str(k), v)
+                else:
+                    raise KeyError('{} is already in modules. Please choose a different name'.format(str(k)))
+
+    def __getitem__(self, idx):
+        return self._modules[str(idx)]
+
+    def __setitem__(self, idx, module):
+        return setattr(self, str(idx), module)
+
+    def __len__(self):
+        return len(self._modules)
+
+    def __iter__(self):
+        return iter(self._modules)
+
+    def keys(self):
+        return self._modules.keys()
+
+    def items(self):
+        return self._modules.items()
+
+    def values(self):
+        return self._modules.values()
 
 class Readout():
     def initialize(self, *args, **kwargs):

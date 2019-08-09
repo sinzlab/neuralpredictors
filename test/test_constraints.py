de4672066c8a056e097ef6a8a1ddbae6d0462c8b
Fabian Sinz <sinz@bcm.edu>
2019-08-09 10:58:54 +0200

    test positivity

--- test/test_constraints.py
+++ test/test_constraints.py
@@ -5,22 +5,25 @@ from mlutils.constraints import positive
 
 class TestPositive:
 
+    tolerance = 1e-8
+
     def test_positive(self):
         w = torch.Tensor([-1, 1.])
         positive(w)
 
         assert torch.all(w >= 0)
 
-    # def test_nograd(self):
-    #     w = torch.Tensor([-1, 1.]).requires_grad_(True)
-    #     v = torch.Tensor([-1, 1.]).requires_grad_(True)
-    #
-    #     positive(w)
-    #     s1 = w.sum()
-    #     s2 = v.sum()
-    #     print(s2)
-    #     s1.backward()
-    #     s2.backward()
-    #     print(s2.gradient)
-    #     print(s1.grad - s2.grad)
-    #     assert torch.all((s1.grad - s2.grad).abs() < 1e-6)
+    def test_nograd(self):
+        w = torch.Tensor([-.1, 1.]).requires_grad_(True)
+        v = torch.Tensor([-.1, 1.]).requires_grad_(True)
+
+        positive(w)
+
+        s1 = (w * w).sum()
+        s2 = (v * v).sum()
+
+        s1.backward()
+        s2.backward()
+        assert (w.grad[-1] - v.grad[-1]).abs() < self.tolerance
+        assert (w.grad[0] - v.grad[0]).abs() > self.tolerance
+

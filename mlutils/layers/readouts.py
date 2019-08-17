7f3533bf26719e6ba7b48fe0292f31e42b2f8dd0
akjagadish <akshaykjagadish@gmail.com>
2019-08-17 17:10:44 +0200

    Update Gaussian Readout
    Signed-off-by: akjagadish <akshaykjagadish@gmail.com>

--- mlutils/layers/readouts.py
+++ mlutils/layers/readouts.py
@@ -1,11 +1,11 @@
 from collections import OrderedDict
 import numpy as np
 import torch
+import warnings
 from torch import nn as nn
 from torch.nn import Parameter
 from torch.nn import functional as F
 from torch.nn import ModuleDict
-
 
 class Readout():
     def initialize(self, *args, **kwargs):
@@ -22,7 +22,7 @@ class Readout():
 
 
 #################
-#Gaussian Readout
+# Gaussian Readout
 ################
 
 class MultipleGaussian2d(Readout, ModuleDict):
@@ -38,6 +38,7 @@ class MultipleGaussian2d(Readout, ModuleDict):
             self.add_module(k, Gaussian2d(in_shape=in_shape, outdims=n_neurons, **kwargs))
 
     def initialize(self, mean_activity_dict):
+
         for k, mu in mean_activity_dict.items():
             self[k].initialize()
             self[k].bias.data = mu.squeeze() - 1
@@ -47,14 +48,39 @@ class MultipleGaussian2d(Readout, ModuleDict):
 
 
 class Gaussian2d(nn.Module):
+    """
+
+    'Gaussian2d' class instantiates an object that can used to learn a point in the core feature space for each neuron,
+    sampled from a Gaussian distribution with some mean and variance at train but set to mean at test time, that best predicts its response.
+
+    The readout receives the shape of the core as 'in_shape', the number of units/neurons being predicted as 'outdims', 'bias' specifying whether
+    or not bias term is to be used and 'init_range' range for initialising the mean and variance of the gaussian distribution from which we sample to
+    uniform distribution, U(-init_range,init_range) and  uniform distribution, U(0.0, 3*init_range) respectively.
+    The grid parameter contains the normalized locations (x, y coordinates in the core feature space) and is clipped to [-1.1] as it a
+    requirement of the torch.grid_sample function. The feature parameter learns the best linear mapping between the feature
+    map from a given location, sample from Gaussian at train time but set to mean at eval time, and the unit's response with or without an additional elu non-linearity.
+
+    Args:
+        in_shape (list): shape of the input feature map [channels, width, height]
+        outdims (int): number of output units
+        bias (bool): adds a bias term
+        init_range (float): intialises the the mean with Uniform([-init_range, init_range])
+                            [expected: positive value <=1]
+                            and sigmal with Uniform([0.0, 3*init_range])
+
+
+    """
+
     def __init__(self, in_shape, outdims, bias, init_range, **kwargs):
+
         super().__init__()
         self.in_shape = in_shape
         c, w, h = in_shape
         self.outdims = outdims
-        self.mu = Parameter(torch.Tensor(1, outdims, 1, 2))       # mean location of gaussian for each neuron
-        self.sigma = Parameter(torch.Tensor(1, outdims, 1, 2))    # standard deviation for gaussian for each neuron
-        self.features = Parameter(torch.Tensor(1, c, 1, outdims)) # saliency  weights for each channel from core
+        self.grid_shape = (1, outdims, 1, 2)
+        self.mu = Parameter(torch.Tensor(1, outdims, 1, 2))  # mean location of gaussian for each neuron
+        self.sigma = Parameter(torch.Tensor(1, outdims, 1, 2))  # standard deviation for gaussian for each neuron
+        self.features = Parameter(torch.Tensor(1, c, 1, outdims))  # saliency  weights for each channel from core
 
         if bias:
             bias = Parameter(torch.Tensor(outdims))
@@ -67,35 +93,43 @@ class Gaussian2d(nn.Module):
 
     @property
     def grid(self):
-
+        """
+        grid function returns the grid location sampled from a Gaussian distribution, N(mu,sigma)
+        """
         if self.training:
-            Normal = torch.distributions.Normal(torch.tensor([0.0]), torch.tensor([1.0]))  # normal distribution with
-            # mean 0, std = 1
-            z = self.mu + self.sigma * Normal.sample((1,readout.outdims,1,2)).squeeze(-1) # reparameterise normal to get the grid
-            return(torch.clamp(z, -1, 1))
+            norm = self.mu.new(*self.grid_shape).normal_()
+            z = norm * torch.clamp(self.sigma, min=0) + torch.clamp(self.mu, min=-1, max=1)
+            return (torch.clamp(z, -1, 1))
+
 
         elif self.eval:
 
-            return(self.mu)
-
+            return (self.mu)
 
     def initialize(self):
-        self.mu.data.uniform_(-self.init_range,self.init_range)
-        self.sigma.data.uniform_(-self.init_range,self.init_range)
+        """
+        initialize function initializes the mean, sigma for the Gaussian readout and features weights
+        """
+        self.mu.data.uniform_(-self.init_range, self.init_range)
+        self.sigma.data.uniform_(0.0, 3 * self.init_range)
         self.features.data.fill_(1 / self.in_shape[0])
+
 
         if self.bias is not None:
             self.bias.data.fill_(0)
 
     def feature_l1(self, average=True):
+        """
+        feature_l1 function returns the l1 regularization term either the mean or just the sum of weights
+        Args:
+            average(bool): if True, use mean of weights for regularization
+        """
         if average:
             return self.features.abs().mean()
         else:
             return self.features.abs().sum()
 
     def forward(self, x, shift=None, out_idx=None):
-
-
         N, c, w, h = x.size()
         feat = self.features.view(1, c, self.outdims)
 
@@ -132,24 +166,25 @@ class Gaussian2d(nn.Module):
             r += '  -> ' + ch.__repr__() + '\n'
         return r
 
+
 ##############
-#Pyramid Readout
+# Pyramid Readout
 ##############
 
 
 class MultiplePointPyramid2d(Readout, ModuleDict):
     def __init__(self, in_shape, loaders, gamma_readout, positive, **kwargs):
-#        log.info('Ignoring input {} when creating {}'.format(repr(kwargs), self.__class__.__name__))
+        #        log.info('Ignoring input {} when creating {}'.format(repr(kwargs), self.__class__.__name__))
         super().__init__()
 
         self.in_shape = in_shape
         self.neurons = OrderedDict([(k, loader.dataset.n_neurons) for k, loader in loaders.items()])
-        self._positive = positive #kwargs['positive']
+        self._positive = positive  # kwargs['positive']
         self.gamma_readout = gamma_readout
         for k, n_neurons in self.neurons.items():
             if isinstance(self.in_shape, dict):
                 in_shape = self.in_shape[k]
-            self.add_module(k, PointPyramid2d(in_shape=in_shape, outdims=n_neurons, positive = positive, **kwargs))
+            self.add_module(k, PointPyramid2d(in_shape=in_shape, outdims=n_neurons, positive=positive, **kwargs))
 
     @property
     def positive(self):
@@ -162,7 +197,7 @@ class MultiplePointPyramid2d(Readout, ModuleDict):
             self[k].positive = value
 
     def initialize(self, mu_dict):
-#        log.info('Initializing with mu_dict: ' + ', '.join(['{}: {}'.format(k, len(m)) for k, m in mu_dict.items()]))
+        #        log.info('Initializing with mu_dict: ' + ', '.join(['{}: {}'.format(k, len(m)) for k, m in mu_dict.items()]))
 
         for k, mu in mu_dict.items():
             self[k].initialize()
@@ -170,7 +205,6 @@ class MultiplePointPyramid2d(Readout, ModuleDict):
 
     def regularizer(self, readout_key):
         return self[readout_key].feature_l1() * self.gamma_readout
-
 
 
 class Pyramid(nn.Module):
@@ -318,9 +352,8 @@ class PointPyramid2d(nn.Module):
         return r
 
 
-
 ##############
-#Point Pooled Readout
+# Point Pooled Readout
 ##############
 
 
@@ -331,9 +364,9 @@ class MultiplePointPool2d(Readout, ModuleDict):
         self.in_shape = in_shape
         self.neurons = OrderedDict([(k, loader.dataset.n_neurons) for k, loader in loaders.items()])
 
-        self.gamma_readout = gamma_readout
+        self.gamma_readout = gamma_readout  # regularisation strength
 
-        for k, n_neurons in self.neurons.items():
+        for k, n_neurons in self.neurons.items():  # example: two areas correspond to two readouts
             self.add_module(k, PointPooled2d(in_shape=in_shape, outdims=n_neurons, **kwargs))
 
     def initialize(self, mean_activity_dict):
@@ -347,13 +380,38 @@ class MultiplePointPool2d(Readout, ModuleDict):
 
 class PointPooled2d(nn.Module):
     def __init__(self, in_shape, outdims, pool_steps, bias, pool_kern, init_range, **kwargs):
+        """
+
+        'PointPooled2d' function learns a point in the core feature space for each neuron, with help of torch.grid_sample, that best
+        predicts its response. Multiple average pooling steps are applied to reduce search space in each stage and thereby, faster convergence to the best prediction point.
+
+        The readout receives the shape of the core as 'in_shape', number of pooling stages to be performed as 'pool_steps', the kernel size and stride length
+        to be used for pooling as 'pool_kern', the number of units/neurons being predicted as 'outdims', 'bias' specifying whether
+        or not bias term is to be used and 'init_range' range for initialising the grid with uniform distribution, U(-init_range,init_range).
+        The grid parameter contains the normalized locations (x, y coordinates in the core feature space) and is clipped to [-1.1] as it a
+        requirement of the torch.grid_sample function. The feature parameter learns the best linear mapping from the pooled feature
+        map from a given location to a unit's response with or without an additional elu non-linearity.
+
+        Args:
+            in_shape (list): shape of the input feature map [channels, width, height]
+            outdims (int): number of output units
+            pool_steps (int): number of pooling stages
+            bias (bool): adds a bias term
+            pool_kern (int): filter size and stride length used for pooling the feature map
+            init_range (float): intialises the grid with Uniform([-init_range, init_range])
+                                [expected: positive value <=1]
+
+        """
         super().__init__()
+        if 0 <= init_range <= 1:
+            raise ValueError("init_range should be a positive number <=1")
         self._pool_steps = pool_steps
         self.in_shape = in_shape
         c, w, h = in_shape
         self.outdims = outdims
-        self.grid = Parameter(torch.Tensor(1, outdims, 1, 2))
+        self.grid = Parameter(torch.Tensor(1, outdims, 1, 2))  # x-y coordinates for each neuron
         self.features = Parameter(torch.Tensor(1, c * (self._pool_steps + 1), 1, outdims))
+        # weight matrix mapping the core features to the output units
 
         if bias:
             bias = Parameter(torch.Tensor(outdims))
@@ -362,7 +420,8 @@ class PointPooled2d(nn.Module):
             self.register_parameter('bias', None)
 
         self.pool_kern = pool_kern
-        self.avg = nn.AvgPool2d((pool_kern, pool_kern), stride=pool_kern, count_include_pad=False)
+        self.avg = nn.AvgPool2d((pool_kern, pool_kern), stride=pool_kern,
+                                count_include_pad=False)  # setup kernel of size=[pool_kern,pool_kern] with stride=pool_kern
         self.init_range = init_range
         self.initialize()
 
@@ -381,6 +440,9 @@ class PointPooled2d(nn.Module):
             self.features.data.fill_(1 / self.in_shape[0])
 
     def initialize(self):
+        """
+        initialize function initialises the grid, features or weights and bias terms.
+        """
         self.grid.data.uniform_(-self.init_range, self.init_range)
         self.features.data.fill_(1 / self.in_shape[0])
 
@@ -388,22 +450,34 @@ class PointPooled2d(nn.Module):
             self.bias.data.fill_(0)
 
     def feature_l1(self, average=True):
+        """
+        feature_l1 function helps is applying l1 regularization.
+        Args:
+            average(bool): if True, use mean of weights for regularization
+
+        """
         if average:
             return self.features.abs().mean()
         else:
             return self.features.abs().sum()
 
     def forward(self, x, shift=None, out_idx=None):
-        self.grid.data = torch.clamp(self.grid.data, -1, 1)
+        self.grid.data = torch.clamp(self.grid.data, -1, 1)  # test if clipping is part of the gradient
         N, c, w, h = x.size()
-        m = self.pool_steps + 1
+        c_in, w_in, h_in = self.in_shape
+        if [c_in, w_in, h_in] != [c, w, h]:
+            raise ValueError("the specified feature map dimension is not the readout's expected input dimension")
+
+        m = self.pool_steps + 1  # the input feature is considered the first pooling stage
         feat = self.features.view(1, m * c, self.outdims)
 
         if out_idx is None:
+            # predict all output units
             grid = self.grid
             bias = self.bias
             outdims = self.outdims
         else:
+            # out_idx specifies the indices to subset of units
             feat = feat[:, :, out_idx]
             grid = self.grid[:, out_idx]
             if self.bias is not None:
@@ -413,10 +487,14 @@ class PointPooled2d(nn.Module):
         if shift is None:
             grid = grid.expand(N, outdims, 1, 2)
         else:
+            # shift grid based on shifter network's prediction
             grid = grid.expand(N, outdims, 1, 2) + shift[:, None, None, :]
 
         pools = [F.grid_sample(x, grid)]
         for _ in range(self.pool_steps):
+            _, _, w_pool, h_pool = x.size()
+            if w_pool * h_pool == 1:
+                warnings.warn('redundant pooling steps: pooled feature map size is already 1X1, consider reducing it')
             x = self.avg(x)
             pools.append(F.grid_sample(x, grid))
         y = torch.cat(pools, dim=1)

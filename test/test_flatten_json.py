814e17204215edea57785de323a2d72653cf9b3a
kklurz <kk.lurz@gmail.com>
2019-09-02 14:22:22 +0200

    Correct failed merge

--- test/test_flatten_json.py
+++ test/test_flatten_json.py
@@ -1,36 +1,25 @@
 from mlutils.utils import flatten_json
 import pytest
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 def nested_dictionary(duplicates):
     dictionary = dict(a=0, dict2=dict(b=0, dict3=dict(c=0)))
     if duplicates:
         dictionary['c'] = 0
     return dictionary
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 def flat_dictionary(keep_nested_name):
     if keep_nested_name:
         return dict(a=0, dict2_b=0, dict2_dict3_c=0)
     else:
         return dict(a=0, b=0, c=0)
 
-<<<<<<< HEAD
+
 @pytest.mark.parametrize('keep_nested_name', [True, False])
 def test_output():
     assert flatten_json(nested_dictionary(duplicates=False), keep_nested_name) == flat_dictionary(keep_nested_name)
 
-=======
-def test_output():
-    for keep_nested_name in (True,False):
-        assert flatten_json(nested_dictionary(duplicates=False), keep_nested_name) == flat_dictionary(keep_nested_name)
->>>>>>> origin/master
 
 def test_exception():
     with pytest.raises(ValueError):

328135e06238932619a9b3eb88f2b2b841f05409
Konstantin Willeke <konstantin.willeke@gmail.com>
2019-11-25 18:27:22 +0100

    Add learning rate decay scheduler to early stopping. Extend early_stopping with an additional argument, to take in a scheduler object. Allows for restoring the best model after each LR decay.

--- mlutils/training.py
+++ mlutils/training.py
@@ -73,9 +73,17 @@ def eval_state(model):
 
 def early_stopping(model, objective_closure, interval=5, patience=20, start=0, max_iter=1000,
                    maximize=True, tolerance=1e-5, switch_mode=True, restore_best=True,
-                   tracker=None):
+                   tracker=None, scheduler=None, lr_decay_steps=1):
     """
-    Early stopping iterator. When it stops, it restores the best previous state of the model.
+    Early stopping iterator. Keeps track of the best model state during training. Resets the model to its
+        best state, when either the number of maximum epochs or the patience [number of epochs without improvement)
+        is reached.
+    Also includes a convenient way to reduce the learning rate. Takes as an additional input a PyTorch scheduler object
+        (e.g. torch.optim.lr_scheduler.ReduceLROnPlateau), which will automatically decrease the learning rate.
+        If the patience counter is reached, the scheduler will decay the LR, and the model is set back to its best state.
+        This loop will continue for n times in the variable lr_decay_steps. The patience and tolerance parameters in
+        early stopping and the scheduler object should be identical.
+
 
     Args:
         model:     model that is being optimized
@@ -94,6 +102,11 @@ def early_stopping(model, objective_closure, interval=5, patience=20, start=0, m
         tracker (Tracker):
             for tracking training time & stopping objective
 
+        scheduler:  scheduler object, which automatically reduces decreases the LR by a specified amount.
+                    The scheduler should be defined outside of early stopping, and should take as inputs the same
+                    arguments for patience, and tolerance, as early stopping
+        lr_decay_steps: Number of times the learning rate should be reduced before stopping the training.
+
     """
     training_status = model.training
 
@@ -104,6 +117,14 @@ def early_stopping(model, objective_closure, interval=5, patience=20, start=0, m
         if switch_mode:
             model.train(training_status)
         return ret
+
+    def decay_lr(model, best_state_dict):
+        old_objective = _objective()
+        if restore_best:
+            model.load_state_dict(best_state_dict)
+            print(
+                'Restoring best model after lr decay! {:.6f} ---> {:.6f}'.format(
+                    old_objective, _objective()))
 
     def finalize(model, best_state_dict):
         old_objective = _objective()
@@ -120,32 +141,43 @@ def early_stopping(model, objective_closure, interval=5, patience=20, start=0, m
     maximize = float(maximize)
     best_objective = current_objective = _objective()
     best_state_dict = copy_state(model)
-    patience_counter = 0
-    while patience_counter < patience and epoch < max_iter:
-        for _ in range(interval):
-            epoch += 1
-            if tracker is not None:
-                tracker.log_objective(current_objective)
-            if (~np.isfinite(current_objective)).any():
-                print('Objective is not Finite. Stopping training')
-                finalize(model, best_state_dict)
-                return
-            yield epoch, current_objective
 
-        current_objective = _objective()
+    for repeat in range(lr_decay_steps):
+        patience_counter = 0
 
-        if current_objective * (-1) ** maximize < best_objective * (-1) ** maximize - tolerance:
-            print('[{:03d}|{:02d}/{:02d}] ---> {}'.format(epoch, patience_counter, patience, current_objective),
-                  flush=True)
-            best_state_dict = copy_state(model)
-            best_objective = current_objective
-            patience_counter = 0
-        else:
-            patience_counter += 1
-            print('[{:03d}|{:02d}/{:02d}] -/-> {}'.format(epoch, patience_counter, patience, current_objective),
-                  flush=True)
+        while patience_counter < patience and epoch < max_iter:
+
+            for _ in range(interval):
+                epoch += 1
+                if tracker is not None:
+                    tracker.log_objective(current_objective)
+                if (~np.isfinite(current_objective)).any():
+                    print('Objective is not Finite. Stopping training')
+                    finalize(model, best_state_dict)
+                    return
+                yield epoch, current_objective
+
+            current_objective = _objective()
+
+            # if a scheduler is defined, a .step with the current objective is all that is needed to reduce the LR
+            if scheduler is not None:
+                scheduler.step(current_objective)
+
+            if current_objective * (-1) ** maximize < best_objective * (-1) ** maximize - tolerance:
+                print('[{:03d}|{:02d}/{:02d}] ---> {}'.format(epoch, patience_counter, patience, current_objective),
+                      flush=True)
+                best_state_dict = copy_state(model)
+                best_objective = current_objective
+                patience_counter = 0
+            else:
+                patience_counter += 1
+                print('[{:03d}|{:02d}/{:02d}] -/-> {}'.format(epoch, patience_counter, patience, current_objective),
+                      flush=True)
+
+        if (epoch < max_iter) & (lr_decay_steps > 1) & (repeat < lr_decay_steps):
+            decay_lr(model, best_state_dict)
+
     finalize(model, best_state_dict)
-
 
 def alternate(*args):
     """

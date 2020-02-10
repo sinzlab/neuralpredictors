1be15f60cbbf02f8739b4cef25753cec2cd9605d
cgruner <50152424+ClaudiusGruner@users.noreply.github.com>
2020-02-10 13:58:00 +0100

    Update flows.py

--- mlutils/layers/flows.py
+++ mlutils/layers/flows.py
@@ -317,20 +317,20 @@ class ConditionalFlow(nn.ModuleList):
 
 
 class Bias2DLayer(nn.Module):
-    def __init__(self,channels,initial=0,**kwargs):
+    def __init__(self, channels, initial=0, **kwargs):
         super(Bias2DLayer, self).__init__(**kwargs)
 
-        self.bias = torch.nn.Parameter(torch.empty((1,channels,1,1)).fill_(initial))
+        self.bias = torch.nn.Parameter(torch.empty((1, channels, 1, 1)).fill_(initial))
 
-    def forward(self,x):
+    def forward(self, x):
         return x + self.bias
     
     
 class Scale2DLayer(nn.Module):
-    def __init__(self,channels,initial=0,**kwargs):
+    def __init__(self, channels, initial=1, **kwargs):
         super(Scale2DLayer, self).__init__(**kwargs)
 
-        self.scale = torch.nn.Parameter(torch.empty((1,channels,1,1)).fill_(initial))
+        self.scale = torch.nn.Parameter(torch.empty((1, channels, 1, 1)).fill_(initial))
 
-    def forward(self,x):
+    def forward(self, x):
         return x * self.scale

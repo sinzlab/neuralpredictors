6065321d065fef4849326122b0136cd0796d2f7f
cgruner <50152424+ClaudiusGruner@users.noreply.github.com>
2020-02-10 13:24:57 +0100

    Update flows.py

--- mlutils/layers/flows.py
+++ mlutils/layers/flows.py
@@ -330,7 +330,7 @@ class Scale2DLayer(nn.Module):
     def __init__(self,channels,initial=0,**kwargs):
         super(Scale2DLayer, self).__init__(**kwargs)
 
-        self.bias = torch.nn.Parameter(torch.empty((1,channels,1,1)).fill_(initial))
+        self.scale = torch.nn.Parameter(torch.empty((1,channels,1,1)).fill_(initial))
 
     def forward(self,x):
-        return x * self.bias
+        return x * self.scale

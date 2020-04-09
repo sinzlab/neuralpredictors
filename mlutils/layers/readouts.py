e59f80fd2c3e928c4c871007d0d5fe7ccd52ca0b
Fabian Sinz <sinz@bcm.edu>
2020-04-09 20:56:35 +0200

    factorized readout

--- mlutils/layers/readouts.py
+++ mlutils/layers/readouts.py
@@ -32,6 +32,77 @@ class Readout:
         return s + "|".join(ret) + "]\n"
 
 
+class SpatialXFeatureLinear(nn.Module):
+    """
+    Factorized fully connected layer. Weights are a sum of outer products between a spatial filter and a feature vector.
+    """
+
+    def __init__(self, in_shape, outdims, bias, normalize=True, init_noise=1e-3, **kwargs):
+        super().__init__()
+        self.in_shape = in_shape
+        self.outdims = outdims
+        self.normalize = normalize
+        c, w, h = in_shape
+        self.spatial = Parameter(torch.Tensor(self.outdims, w, h))
+        self.features = Parameter(torch.Tensor(self.outdims, c))
+        self.init_noise = init_noise
+        if bias:
+            bias = Parameter(torch.Tensor(self.outdims))
+            self.register_parameter('bias', bias)
+        else:
+            self.register_parameter('bias', None)
+        self.initialize()
+
+    @property
+    def normalized_spatial(self):
+        positive(self.spatial)
+        if self.normalize:
+            norm = self.spatial.pow(2).sum(dim=2, keepdim=True)
+            norm = norm.sum(dim=3, keepdim=True).sqrt().expand_as(self.spatial) + 1e-6
+            weight = self.spatial / norm
+        else:
+            weight = self.spatial
+        return weight
+
+    # @property
+    # def weight(self):
+    #     if self.positive:
+    #         positive(self.features)
+    #     n = self.outdims
+    #     c, w, h = self.in_shape
+    #     weight = self.normalized_spatial.expand(n, c, w, h) * self.features.expand(n, c, w, h)
+    #     weight = weight.view(self.outdims, -1)
+    #     return weight
+
+    def l1(self, average=False):
+        n = self.outdims
+        c, w, h = self.in_shape
+        ret = (self.normalized_spatial.view(self.outdims, -1).abs().sum(dim=1, keepdim=True)
+               * self.features.view(self.outdims, -1).abs().sum(dim=1)).sum()
+        if average:
+            ret = ret / (n * c * w * h)
+        return ret
+
+    def initialize(self, init_noise=1e-3):
+        self.spatial.data.normal_(0, self.init_noise)
+        self.features.data.normal_(0, self.init_noise)
+        if self.bias is not None:
+            self.bias.data.fill_(0)
+
+    def forward(self, x, shift=None):
+        y = torch.einsum('ncwh,owh->nco', x, self.normalized_spatial)
+        y = torch.einsum('nco,oc->no', y, self.features)
+        if self.bias is not None:
+            y = y + self.bias
+        return y
+
+    def __repr__(self):
+        return ('normalized ' if self.normalize else '') + \
+               self.__class__.__name__ + \
+               ' (' + '{} x {} x {}'.format(*self.in_shape) + ' -> ' + str(
+            self.outdims) + ')'
+
+
 class ClonedReadout(Readout, nn.Module):
     """
     This readout clones another readout while applying a linear transformation on the output. Used for MultiDatasets
@@ -1011,7 +1082,6 @@ class Gaussian3d(nn.Module):
         return y
 
 
-
 class UltraSparse(nn.Module):
     """
     This readout instantiates an object that can used to learn one or more features (with or without
@@ -1344,6 +1414,7 @@ class MultipleFullGaussian2d(MultiReadout):
     """
 
     _base_readout = FullGaussian2d
+
 
 class MultipleUltraSparse(MultiReadout):
     """

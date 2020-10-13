b62aed973decf866cd38f639b5a7a1841ea7d722
Edgar Y. Walker <edgar.walker@gmail.com>
2020-10-13 17:21:52 +0200

    Make release of v0.0.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "requirements.txt")) as f:
 
 setup(
     name="neuralpredictors",
-    version="0.0.0rc3",
+    version="0.0.0",
     description="Sinz Lab Neural System Identification Utilities",
     author="Sinz Lab",
     author_email="software@sinzlab.net",

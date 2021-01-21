ac6246c9b65b094df974da5e6665bf0673aed25a
kklurz <46533338+kklurz@users.noreply.github.com>
2021-01-21 16:56:27 +0100

    blackening things out

--- neuralpredictors/utils.py
+++ neuralpredictors/utils.py
@@ -121,8 +121,9 @@ class BiasNet(nn.Module):
 class HiddenPrints:
     def __enter__(self):
         self._original_stdout = sys.stdout
-        sys.stdout = open(os.devnull, 'w')
+        sys.stdout = open(os.devnull, "w")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
         sys.stdout = self._original_stdout
+

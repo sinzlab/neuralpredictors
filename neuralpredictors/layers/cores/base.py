7f4cf391fcc144ad8f7852b70bce44a9bb3d97b6
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2021-03-30 12:26:26 +0200

    Clean up core2d base class and adapt the models and imports accordingly

--- neuralpredictors/layers/cores/base.py
+++ neuralpredictors/layers/cores/base.py
@@ -1,23 +1,15 @@
-import warnings
-from collections import OrderedDict, Iterable
-
 from torch import nn
-import torch
-import torchvision
-
-from .. import regularizers
-from . import Bias2DLayer, Scale2DLayer
-from .activations import AdaptiveELU
-from .hermite_layers import (
-    HermiteConv2D,
-    RotationEquivariantBatchNorm2D,
-    RotationEquivariantBias2DLayer,
-    RotationEquivariantScale2DLayer,
-)
 
 
 class Core:
+    """
+    Base class for the core models where shared features (across neurons) are computed.
+    """
+
     def initialize(self):
+        """
+        Initialization applied on the core.
+        """
         raise NotImplementedError("Not initializing")
 
     def __repr__(self):
@@ -30,22 +22,42 @@ class Core:
 
 
 class Core2d(Core):
-    def initialize(self, cuda=False):
-        self.apply(self.init_conv)
-        self.put_to_cuda(cuda=cuda)
+    """
+    Base class for the core models, taking 2d inputs and computing nonlinear features.
+    """
 
-    def put_to_cuda(self, cuda):
-        if cuda:
-            self = self.cuda()
+    def initialize(self):
+        """
+        Initialization applied on the core.
+        """
+        self.apply(self.init_conv)
 
     @staticmethod
     def init_conv(m):
+        """
+        Initialize convolution layers with:
+            - weights: xavier_normal
+            - biases: 0
+
+        Args:
+            m (nn.Module): a pytorch nn module.
+        """
         if isinstance(m, nn.Conv2d):
             nn.init.xavier_normal_(m.weight.data)
             if m.bias is not None:
                 m.bias.data.fill_(0)
 
-    @staticmethod
-    def init_conv_hermite(m):
-        if isinstance(m, HermiteConv2D):
-            nn.init.normal_(m.coeffs.data, std=0.1)
+    def forward(self, x):
+        """
+        Forward function for pytorch nn module.
+
+        Args:
+            x (torch.tensor): input of shape (batch, channels, height, width)
+        """
+        raise NotImplementedError()
+
+    def regularizer(self):
+        """
+        Regularization applied on the core. Returns a scalar value.
+        """
+        raise NotImplementedError()

71abc9cccb8c5a70fad52d986fc6cc2f5a2952ac
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2021-03-30 15:04:26 +0200

    Edit encoder description

--- neuralpredictors/layers/__init__.py
+++ neuralpredictors/layers/__init__.py
@@ -3,7 +3,7 @@ This package contains all the components that are used to construct system ident
 This includes:
   - cores: Modules used for extracting nonlinearly computed features shared among neurons.
   - readouts: Linear layers added on top of the core output to predict neurons' responses.
-  - encoders: Classes which fuse core and readout together into a single model.
+  - encoders: Classes which fuse core, readout, shifter (optional), and modulator (optional) into a single model.
   - shifters: Modules used for shifting (i.e. spatially) the features computed via core.
   - modulators: Modules used to incorporate the effect of other variables (e.g. behavioral variables) in response prediction.
   - special layers (e.g. DepthSeparable Convolution) and activation functions.

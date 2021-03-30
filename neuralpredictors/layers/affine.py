82b2fd7e41edefe7ee76fa2fb75cf6b09cde5fae
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2021-03-30 13:47:48 +0200

    Add docstring to affine.py module

--- neuralpredictors/layers/affine.py
+++ neuralpredictors/layers/affine.py
@@ -3,7 +3,16 @@ from torch import nn
 
 
 class Bias2DLayer(nn.Module):
+    """
+    Bias per channel for a given 2d input.
+    """
+
     def __init__(self, channels, initial=0, **kwargs):
+        """
+        Args:
+            channels (int): number of channels in the input.
+            initial (int, optional): intial value. Defaults to 0.
+        """
         super(Bias2DLayer, self).__init__(**kwargs)
 
         self.bias = torch.nn.Parameter(torch.empty((1, channels, 1, 1)).fill_(initial))
@@ -13,7 +22,16 @@ class Bias2DLayer(nn.Module):
 
 
 class Scale2DLayer(nn.Module):
+    """
+    Scale per channel for a given 2d input.
+    """
+
     def __init__(self, channels, initial=1, **kwargs):
+        """
+        Args:
+            channels (int): number of channels in the input.
+            initial (int, optional): intial value. Defaults to 1.
+        """
         super(Scale2DLayer, self).__init__(**kwargs)
 
         self.scale = torch.nn.Parameter(torch.empty((1, channels, 1, 1)).fill_(initial))

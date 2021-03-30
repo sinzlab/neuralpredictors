aab71b18e4236bd6d3aa4444e1bcd3b4d3e8c432
KonstantinWilleke <konstantin.willeke@gmail.com>
2021-03-30 15:42:21 +0200

    Extend documentation.

--- neuralpredictors/layers/shifters/mlp.py
+++ neuralpredictors/layers/shifters/mlp.py
@@ -1,16 +1,24 @@
 from .base import Shifter
 
-from torch import nn
 from torch.nn.init import xavier_normal
 from torch.nn import ModuleDict
+from torch import nn
 
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-class MLP(nn.Module):
+class MLP(Shifter):
     def __init__(self, input_features=2, hidden_channels=10, shift_layers=1, **kwargs):
+        """
+        Multi-layer perceptron shifter
+        Args:
+            input_features (int): number of input features, defaults to 2.
+            hidden_channels (int): number of hidden units.
+            shift_layers(int): number of hidden layers.
+            **kwargs:
+        """
         super().__init__()
 
         feat = []
@@ -40,6 +48,13 @@ class MLPShifter(Shifter, ModuleDict):
     def __init__(
         self, data_keys, input_channels=2, hidden_channels_shifter=2, shift_layers=1, gamma_shifter=0, **kwargs
     ):
+        """
+        Args:
+            data_keys (list of str): keys of the shifter dictionary, correspond to the data_keys of the nnfabirk dataloaders
+            gamma_shifter: weight of the regularizer
+
+            See docstring of base class for the other arguments.
+        """
         super().__init__()
         self.gamma_shifter = gamma_shifter
         for k in data_keys:

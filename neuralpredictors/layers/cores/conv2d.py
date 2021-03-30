652b06a73601832396430dba2a16ce6110b93cf2
KonstantinWilleke <konstantin.willeke@gmail.com>
2021-03-30 21:00:58 +0200

    Add SE2dCore to conv2d.py.

--- neuralpredictors/layers/cores/conv2d.py
+++ neuralpredictors/layers/cores/conv2d.py
@@ -7,6 +7,9 @@ import torchvision
 
 from .base import Core2d
 from ... import regularizers
+from ..conv import DepthSeparableConv2d
+from ..squeeze_excitation import SqueezeExcitationBlock
+from ..attention import AttentionConv
 from ..affine import Bias2DLayer, Scale2DLayer
 from ..activations import AdaptiveELU
 from ..hermite import (
@@ -542,3 +545,165 @@ class TransferLearningCore(Core2d, nn.Module):
         logger.warning(
             "Ignoring initialization since the parameters should be acquired from a pretrained model. If you want random weights, set pretrained = False."
         )
+
+
+class SE2dCore(Core2d, nn.Module):
+    def __init__(
+        self,
+        input_channels,
+        hidden_channels,
+        input_kern,
+        hidden_kern,
+        layers=3,
+        gamma_input=0.0,
+        skip=0,
+        final_nonlinearity=True,
+        bias=False,
+        momentum=0.1,
+        pad_input=True,
+        batch_norm=True,
+        hidden_dilation=1,
+        laplace_padding=None,
+        input_regularizer="LaplaceL2norm",
+        stack=None,
+        se_reduction=32,
+        n_se_blocks=0,
+        depth_separable=False,
+        attention_conv=False,
+        linear=False,
+    ):
+        """
+        Args:
+            input_channels:     Integer, number of input channels as in
+            hidden_channels:    Number of hidden channels (i.e feature maps) in each hidden layer
+            input_kern:     kernel size of the first layer (i.e. the input layer)
+            hidden_kern:    kernel size of each hidden layer's kernel
+            layers:         number of layers
+            gamma_input:    regularizer factor for the input weights (default: LaplaceL2, see neuralpredictors.regularizers)
+            skip:           Adds a skip connection
+            final_nonlinearity: Boolean, if true, appends an ELU layer after the last BatchNorm (if BN=True)
+            elu_xshift, elu_yshift: final_nonlinearity(x) = Elu(x - elu_xshift) + elu_yshift
+            bias:           Adds a bias layer.
+            momentum:       BN momentum
+            pad_input:      Boolean, if True, applies zero padding to all convolutions
+            batch_norm:     Boolean, if True appends a BN layer after each convolutional layer
+            hidden_dilation:    If set to > 1, will apply dilated convs for all hidden layers
+            laplace_padding: Padding size for the laplace convolution. If padding = None, it defaults to half of
+                the kernel size (recommended). Setting Padding to 0 is not recommended and leads to artefacts,
+                zero is the default however to recreate backwards compatibility.
+            input_regularizer:  String that must match one of the regularizers in ..regularizers
+            stack:        Int or iterable. Selects which layers of the core should be stacked for the readout.
+                            default value will stack all layers on top of each other.
+                            Implemented as layers_to_stack = layers[stack:]. thus:
+                                stack = -1 will only select the last layer as the readout layer.
+                                stack of -2 will read out from the last two layers.
+                                And stack of 1 will read out from layer 1 (0 indexed) until the last layer.
+            se_reduction:   Int, Reduction of channels for global pooling of the Squeeze and Excitation Block.
+            n_se_blocks:    Int, number of squeeze and excitation blocks. Inserted from the last layer
+                              Examples: layers=4, n_se_blocks=2:
+                                => layer0 -> layer1->layer2->SEblock->layer3->SEblock
+            depth_separable: Boolean, if True, uses depth-separable convolutions in all layers after the first one.
+            attention_conv: Boolean, if True, uses self-attention instead of convolution for all layers after the first one.
+            linear:         Boolean, if True, removes all nonlinearities
+        """
+
+        super().__init__()
+
+        assert not bias or not batch_norm, "bias and batch_norm should not both be true"
+        assert not depth_separable or not attention_conv, "depth_separable and attention_conv should not both be true"
+
+        regularizer_config = (
+            dict(padding=laplace_padding, kernel=input_kern)
+            if input_regularizer == "GaussianLaplaceL2"
+            else dict(padding=laplace_padding)
+        )
+        self._input_weights_regularizer = regularizers.__dict__[input_regularizer](**regularizer_config)
+
+        self.layers = layers
+        self.gamma_input = gamma_input
+        self.input_channels = input_channels
+        self.hidden_channels = hidden_channels
+        self.skip = skip
+        self.features = nn.Sequential()
+        self.n_se_blocks = n_se_blocks
+        if stack is None:
+            self.stack = range(self.layers)
+        else:
+            self.stack = [*range(self.layers)[stack:]] if isinstance(stack, int) else stack
+
+        # --- first layer
+        layer = OrderedDict()
+        layer["conv"] = nn.Conv2d(
+            input_channels, hidden_channels, input_kern, padding=input_kern // 2 if pad_input else 0, bias=bias
+        )
+        if batch_norm:
+            layer["norm"] = nn.BatchNorm2d(hidden_channels, momentum=momentum)
+        if (layers > 1 or final_nonlinearity) and not linear:
+            layer["nonlin"] = nn.ELU(inplace=True)
+        self.features.add_module("layer0", nn.Sequential(layer))
+
+        if not isinstance(hidden_kern, Iterable):
+            hidden_kern = [hidden_kern] * (self.layers - 1)
+
+        # --- other layers
+        for l in range(1, self.layers):
+            layer = OrderedDict()
+            hidden_padding = ((hidden_kern[l - 1] - 1) * hidden_dilation + 1) // 2
+            if depth_separable:
+                layer["ds_conv"] = DepthSeparableConv2d(
+                    hidden_channels,
+                    hidden_channels,
+                    kernel_size=hidden_kern[l - 1],
+                    dilation=hidden_dilation,
+                    padding=hidden_padding,
+                    bias=False,
+                    stride=1,
+                )
+            elif attention_conv:
+                layer["conv"] = AttentionConv(
+                    hidden_channels if not skip > 1 else min(skip, l) * hidden_channels,
+                    hidden_channels,
+                    hidden_kern[l - 1],
+                    padding=hidden_padding,
+                    bias=bias and not batch_norm,
+                )
+            else:
+                layer["conv"] = nn.Conv2d(
+                    hidden_channels if not skip > 1 else min(skip, l) * hidden_channels,
+                    hidden_channels,
+                    hidden_kern[l - 1],
+                    padding=hidden_padding,
+                    bias=bias,
+                    dilation=hidden_dilation,
+                )
+            if batch_norm:
+                layer["norm"] = nn.BatchNorm2d(hidden_channels, momentum=momentum)
+
+            if (final_nonlinearity or l < self.layers - 1) and not linear:
+                layer["nonlin"] = nn.ELU(inplace=True)
+
+            if (self.layers - l) <= self.n_se_blocks:
+                layer["seg_ex_block"] = SqueezeExcitationBlock(in_ch=hidden_channels, reduction=se_reduction)
+
+            self.features.add_module("layer{}".format(l), nn.Sequential(layer))
+
+        self.apply(self.init_conv)
+
+    def forward(self, input_):
+        ret = []
+        for l, feat in enumerate(self.features):
+            do_skip = l >= 1 and self.skip > 1
+            input_ = feat(input_ if not do_skip else torch.cat(ret[-min(self.skip, l) :], dim=1))
+            if l in self.stack:
+                ret.append(input_)
+        return torch.cat(ret, dim=1)
+
+    def laplace(self):
+        return self._input_weights_regularizer(self.features[0].conv.weight)
+
+    def regularizer(self):
+        return self.gamma_input * self.laplace()
+
+    @property
+    def outchannels(self):
+        return len(self.features) * self.hidden_channels

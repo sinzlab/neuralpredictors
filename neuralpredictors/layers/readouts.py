cc7b19cb1190fc8cd88055f939c30dc73363b78d
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2021-04-09 21:12:40 +0200

    Make the args of grid_mean_predictor more flexible (Add ReLU and Clamp)

--- neuralpredictors/layers/readouts.py
+++ neuralpredictors/layers/readouts.py
@@ -832,20 +832,35 @@ class FullGaussian2d(nn.Module):
                 max=1,
             )  # grid locations in feature space sampled randomly around the mean self.mu
 
-    def init_grid_predictor(self, source_grid, hidden_features=20, hidden_layers=0, final_tanh=False):
+    def init_grid_predictor(
+        self, source_grid, hidden_features=20, hidden_layers=0, nonlinearity="ELU", final_nonlinearity=None
+    ):
+        class Clamp(nn.Module):
+            def __init__(self, min_val, max_val):
+                super().__init__()
+                self.min_val = min_val
+                self.max_val = max_val
+
+            def forward(self, x):
+                return x.clamp(self.min_val, self.max_val)
+
+        nonlinearities = {"ELU": nn.ELU, "ReLU": nn.ReLU}
+        final_nonlinearities = {"Tanh": nn.Tanh(), "Clamp": Clamp(-1, 1)}
+
         self._original_grid = False
         layers = [nn.Linear(source_grid.shape[1], hidden_features if hidden_layers > 0 else 2)]
 
         for i in range(hidden_layers):
             layers.extend(
                 [
-                    nn.ELU(),
+                    nonlinearities[nonlinearity](),
                     nn.Linear(hidden_features, hidden_features if i < hidden_layers - 1 else 2),
                 ]
             )
 
-        if final_tanh:
-            layers.append(nn.Tanh())
+        if final_nonlinearity is not None:
+            layers.append(final_nonlinearities[final_nonlinearity])
+
         self.mu_transform = nn.Sequential(*layers)
 
         source_grid = source_grid - source_grid.mean(axis=0, keepdims=True)

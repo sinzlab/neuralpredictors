fc633d8c39799fed6aca56183830386266c60847
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2021-04-09 21:55:25 +0200

    apply clamp on the data (not tensor)

--- neuralpredictors/layers/readouts.py
+++ neuralpredictors/layers/readouts.py
@@ -842,7 +842,7 @@ class FullGaussian2d(nn.Module):
                 self.max_val = max_val
 
             def forward(self, x):
-                return x.clamp(self.min_val, self.max_val)
+                x.data.clamp_(self.min_val, self.max_val)
 
         nonlinearities = {"ELU": nn.ELU, "ReLU": nn.ReLU}
         final_nonlinearities = {"Tanh": nn.Tanh(), "Clamp": Clamp(-1, 1)}

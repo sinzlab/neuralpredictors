26604f8c29ca4a25fde3b71d85f720120e6f4407
kklur <kk.lurz@gmail.com>
2021-06-16 11:58:16 +0200

    specify exact imports

--- neuralpredictors/layers/readouts/multi_readout.py
+++ neuralpredictors/layers/readouts/multi_readout.py
@@ -1,10 +1,10 @@
 import torch
-from .base import *
-from .point_pooled import *
-from .gaussian import *
-from .factorized import *
-from .attention import *
-from .pyramid import *
+from .base import Readout, ClonedReadout
+from .point_pooled import PointPooled2d, SpatialTransformerPooled3d
+from .gaussian import Gaussian2d, FullGaussian2d, RemappedGaussian2d, DeterministicGaussian2d, Gaussian3d, UltraSparse
+from .factorized import SpatialXFeatureLinear, FullFactorized2d, FullSXF
+from .attention import AttentionReadout
+from .pyramid import PointPyramid2d
 
 
 class MultiReadoutBase(torch.nn.ModuleDict):

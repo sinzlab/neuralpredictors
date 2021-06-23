eb442d4a287c39ddfb0f5d4a79cb9db1aac45d14
kklur <kk.lurz@gmail.com>
2021-06-23 16:05:08 +0200

    Add docstring

--- neuralpredictors/layers/encoders/poisson.py
+++ neuralpredictors/layers/encoders/poisson.py
@@ -2,7 +2,15 @@ from torch import nn
 
 
 class PoissonEncoder(nn.Module):
-    def __init__(self, core, readout, elu_offset=0, shifter=None):
+    def __init__(self, core, readout, shifter=None, elu_offset=0.0):
+        """
+        An Encoder that wraps the core, readout and optionally a shifter into one model.
+        Args:
+            core (nn.Module): Core model. Refer to neuralpredictors.layers.cores
+            readout (nn.ModuleDict): MultiReadout model. Refer to neuralpredictors.layers.readouts
+            elu_offset (float): Offset value in the final elu non-linearity. Defaults to 0.
+            shifter (optional[nn.ModuleDict]): Shifter network. Refer to neuralpredictors.layers.shifters. Defaults to None.
+        """
         super().__init__()
         self.core = core
         self.readout = readout

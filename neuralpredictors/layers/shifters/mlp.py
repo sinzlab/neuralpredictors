1ec77fbd3b6fed41b6fbdaf62247c33e9af1ba61
kklur <kk.lurz@gmail.com>
2021-07-22 10:24:02 +0200

    add comments to explain shifter code

--- neuralpredictors/layers/shifters/mlp.py
+++ neuralpredictors/layers/shifters/mlp.py
@@ -39,7 +39,9 @@ class MLP(Shifter):
             xavier_normal(linear_layer.weight)
 
     def forward(self, pupil_center, trial_idx=None):
+        # Concatenate pupil center and trial index if trial index is specified
         if trial_idx is not None:
+            # check if the mlp was built for this 3D concatenated input (2D for pupil center, 1D for trial index)
             if self.mlp[0].in_features == 3:
                 pupil_center = torch.cat((pupil_center, trial_idx), dim=1)
         return self.mlp(pupil_center)

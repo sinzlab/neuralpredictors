669365a22ae93796e36d912763ab565c4ebebda6
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2021-12-15 17:34:06 +0100

    Add LongCycler to the __init__ method for import backwards compatibility

--- neuralpredictors/training/__init__.py
+++ neuralpredictors/training/__init__.py
@@ -11,3 +11,4 @@ This includes:
 from .context_managers import eval_state, device_state
 from .early_stopping import early_stopping
 from .tracking import TimeObjectiveTracker, MultipleObjectiveTracker
+from .cyclers import LongCycler
